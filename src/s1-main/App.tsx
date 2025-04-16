b0ce009e8e398547fb9597ae58c46c7de818e4e0
Adam <adam@MacBook-Air-Adam.local>
2025-04-16 22:35:46 +0300

    VV

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -6,8 +6,8 @@ import s from "./App.module.css";
 // import HW3 from "../s2-homeworks/hw03/HW3";
 // import HW4 from "../s2-homeworks/hw04/HW4";
 import HW5 from "../s2-homeworks/hw05/HW5";
- import HW6 from "../s2-homeworks/hw06/HW6";
-import HW7 from "../s2-homeworks/hw07/HW7";
+// import HW6 from "../s2-homeworks/hw06/HW6";
+// import HW7 from "../s2-homeworks/hw07/HW7";
 
 function App() {
   return (
@@ -18,9 +18,9 @@ function App() {
       <HW4 /> */}
 
       {/* при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5 */}
-        <HW5 /> 
-      <HW6 /> 
-      <HW7 />
+      <HW5 />
+      {/* <HW6 />
+      <HW7 /> */}
     </div>
   );
 }
